d3684d3276c86c15d6c686565fff1fac32db13f4
LisaGumbo <lisa.gumbo.ee83@statefarm.com>
2021-12-08 11:55:43 -0600

    adding bucket and changing acl public access to read

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ import { s3Client } from "./client.js";
 
 // Set the parameters
 const params = {
-  Bucket: "<BUCKET_NAME>", // The name of the bucket. For example, 'sample_bucket_101'.
-  Key: "<FILENAME>", // The name of the object. For example, 'sample_upload.txt'.
-  Body: "<BODY_CONTENT>", // The content of the object. For example, 'Hello world!".
+  Bucket: "lisa-bucket-second-practice-c2", // The name of the bucket. For example, 'sample_bucket_101'.
+  Key: "second_practice.txt", // The name of the object. For example, 'sample_upload.txt'.
+  Body: "Hello Lisa, this is your second practice of creating a bucket using an API call", // The content of the object. For example, 'Hello world!".
+  ACL: 'public-read',
 };
 
 const run = () => {
